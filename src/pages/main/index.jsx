9e5a6b5c0bca66db83d9bf44e66aa3414128ca80
MarcoOlivette <marco.vieira.olivette@gmail.com>
2020-09-16 21:25:11 -0300

    learning about navegation react

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Api from "../../services/api";
 import "./style.css";
+import {Link} from 'react-router-dom'
 
 export default class Main extends Component {
   state = {
@@ -27,8 +28,8 @@ export default class Main extends Component {
   };
 
   prevPage = () => {
-    const { page, productInfo} = this.state;
-    
+    const { page} = this.state;
+
     if(page === 1){
         return;
     }
@@ -48,7 +49,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
 
     return (
       <div className="product-list">
@@ -56,14 +57,12 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            <a href={product.url} target="_.blanket">
-              Acessar
-            </a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className="actions">
-          <button onClick={this.prevPage}>Anterior</button>
-          <button onClick={this.nextPage}>Proximo</button>
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Proximo</button>
         </div>
       </div>
     );
